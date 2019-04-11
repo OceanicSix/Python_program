0a7ca0116647e81e7ca2c681e036b658a9b207f7
sean <seanyang89757@gmail.com>
2019-04-11 13:17:19 +1000

    vm

--- eos/input_record.py
+++ eos/input_record.py
@@ -10,12 +10,9 @@ key=input("please input encryption key: ")
 
 encryption=AES_encryption(key)
 ciphertext=encryption.encrypt_string(grade)
-#print(str(ciphertext)[2:-1])
-encrypted_record=[first_name,last_name,str(ciphertext)]
-# print(encrypted_record)
 
 
 command="cleos push action student upsert '["\
-        +'"'+user_name+'"'+","+'"'+first_name+'"'+","+'"'+last_name+'"'+","+'"'+str(ciphertext)+'"'+"]' -p "+user_name+"@active"
+        +'"'+user_name+'"'+","+'"'+first_name+'"'+","+'"'+last_name+'"'+","+'"'+ciphertext.decode("utf-8")+'"'+"]' -p "+user_name+"@active"
 print(command)
 os.system(command)
