753d16daf5bc80bcf181e484062bc894592ff544
Ocean <seanyang89757@gmail.com>
2019-04-29 17:53:35 +1000

    ssd

--- Assignment/MongoDb_Ass2/Query.py
+++ Assignment/MongoDb_Ass2/Query.py
@@ -18,7 +18,24 @@ hotspot_historic=db.hotspot_historic
 # myprint(result)
 
 #Q2.b
-result=hotspot_historic.find({"$and":[{"surface_temperature_celcius":{"$gt":65}},
-                                      {"surface_temperature_celcius":{"$lt":100}}]})
+# result=hotspot_historic.find({"$and":[{"surface_temperature_celcius":{"$gt":65}},
+#                                       {"surface_temperature_celcius":{"$lt":100}}]},
+#                              {"_id":0,"latitude":1,"longitude":1,"surface_temperature_celcius":1,"confidence":1})
+#
+# myprint(result)
 
+#Q2.c
+
+result=climate_historic.aggregate([{"$match":{"$or":[{"date":"15/12/2017"},{"date":"16/12/2017"}]}},
+                                   {"$lookup":
+                                        {
+                                            "from":"hotspot_historic",
+                                            "localField":"date",
+                                            "foreignField":"date",
+                                            "as":"join_result"
+                                        }},
+                                   {"$project":{"_id":0,"date":1,"air_temperature_celcius":1,"relative_humidity":1,
+                                                "max_wind_speed":1,"join_result.surface_temperature_celcius":1}}
+
+])
 myprint(result)
